8c208be8df50d1220eea8171ee472df6ec911484
fabricio <fgbravo188@gmail.com>
2020-05-26 00:03:39 -0300

    Refactors blocks of best_seat_finder.rb

--- app/services/best_seat_finder.rb
+++ app/services/best_seat_finder.rb
@@ -8,14 +8,37 @@ class BestSeatFinder
   end
 
   def find
-    ordered_seats = @seats.sort_by do |seat|
-      [seat.row, seat.column]
-    end
-    selected_row = ordered_seats.group_by(&:row).first
-    selected_row.last.min do |row_seat, next_row_seat|
+    ordered_seats = @seats.sort_by { |seat| [seat.row, seat.column] }
 
-      best_seat_position = (@venue.columns / 2).floor
-      (best_seat_position - row_seat.column).abs <=> (best_seat_position - next_row_seat.column).abs
+    selected_row = ordered_seats.group_by(&:row).find { |_group, row| sequential_available_seats_for_row(row) }
+
+    raise BestSeatFinders::NoAvailableSeatsError if selected_row.nil?
+
+    possible_sets = possible_sets(selected_row.last)
+
+    select_best_set(possible_sets)
+  end
+
+  def sequential_available_seats_for_row(row)
+    return true if @requested_seats.eql?(1)
+
+    row.map(&:column).each_cons(@requested_seats).select { |x, y| y == x.next }.flatten.count >= @requested_seats
+  end
+
+  def possible_sets(row)
+    row.combination(@requested_seats).select { |arr| (arr.last.column.to_i - arr.first.column.to_i) == (@requested_seats - 1) && arr.size == @requested_seats }
+  end
+
+  def select_best_set(sets)
+    sets.min do |set|
+      (set.last.column.to_i - best_seat_position) + (set.first.column.to_i - best_seat_position) - DISRUPTIVE_FACTOR
     end
+  end
+
+  private
+  DISRUPTIVE_FACTOR = 0.01
+
+  def best_seat_position
+    @best_seat_position ||= (@venue.columns.to_f / 2.0)
   end
 end
